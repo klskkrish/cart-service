50464292b820b8a3ed4a96f368a8f74d7d75fbf2
Suresh Krishantha <klskkrish@gmail.com>
2020-02-20 17:44:47 +0530

    test (junit) : add Hikaripool test script

--- src/test/java/com/barter/cart/CartServiceApplicationTests.java
+++ src/test/java/com/barter/cart/CartServiceApplicationTests.java
@@ -1,13 +1,44 @@
 package com.barter.cart;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
-class CartServiceApplicationTests {
+public class CartServiceApplicationTests {
+	private static final Logger logger = LoggerFactory.getLogger(CartServiceApplicationTests.class);
+
+	@Autowired
+	private DataSource dataSource;
 
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
+		logger.info(dataSource.toString());
+		if (dataSource.toString().contains("HikariDataSource")) {
+			logger.info("*****************valid data source assigned*****************");
+			try {
+				dataSource.getConnection().createStatement().execute("select 1");
+				fail();
+			} catch (SQLException e) {
+				logger.error("*****************valid data source assigned*****************");
+				logger.error(e.getMessage(), e);
+				fail();
+			}
+		} else {
+			logger.error("*****************invalid data source assigned*****************");
+			fail();
+		}
 	}
-
 }
